4189cfdb9d693b291131b9b9709ea540a9dfd56d
litao <18202886913@163.com>
2018-09-06 14:35:11 +0800

    创建项目

---
+++ frontend/src/routes.js
@@ -0,0 +1,58 @@
+/* eslint-disable */
+const NotFound = resolve => require(['./views/common/404.vue'], resolve);
+const About = resolve => require(['./views/about.vue'], resolve);
+const MHome = resolve => require(['./views/Home.vue'], resolve);
+const blackList = resolve => require(['./views/blackList/blackList.vue'], resolve);
+const FeedView = resolve => require(['./views/FeedView/FeedView.vue'], resolve);
+const Links = resolve => require(['./views/Resource/Resource.vue'], resolve);
+
+let routes = [
+    {
+      path: '/404',
+      component: NotFound,
+      name: '',
+      hidden: true,
+      M: true,
+      projectHidden: true
+    },
+    {
+      path: '/home',
+      component: MHome,
+      name: '',
+      projectHidden: true,
+      M: true,
+      children: [
+          // { path: '/m/blackList', component: blackList, iconCls:'el-icon-bell', name: '先人指路'},
+          // { path: '/m/FeedView', component: FeedView, iconCls:'el-icon-message', name: '金玉良言'},
+          // { path: '/m/Resource', component: Links, iconCls:'el-icon-share', name: '无私奉献'},
+          // { path: '/m/About', component: About, iconCls:'el-icon-info', name: '关于我们'},
+          // { path: '/robot', component: robot, iconCls:'fa fa-id-card-o', name: '消息机器人', meta: { keepAlive: false }},
+          ]
+    },
+    {
+      path: '/blackList',
+      component: blackList,
+      iconCls:'el-icon-bell',
+      name: '先人指路',
+    },
+    {
+      path: '/FeedView',
+      component: FeedView,
+      iconCls:'el-icon-message',
+      name: '金玉良言'
+    },
+    {
+      path: '/Resource',
+      component: Links,
+      iconCls:'el-icon-share',
+      name: '无私奉献'
+    },
+    {
+      path: '/About',
+      component: About,
+      iconCls:'el-icon-info',
+      name: '关于我们'
+    },
+];
+
+export default routes;
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <mt-header fixed title="成都软件测试" style="height: 60px;font-size: 20px"></mt-header>
+    <div style="margin-top: 70px">
+      <!--<template v-for="(item) in $router.options.routes" v-if="!item.M">-->
+        <mt-cell v-for="(item) in $router.options.routes" v-if="!item.M" :index="item.path" :key="item.path" :title=item.name :to="item.path" is-link>
+          <div slot="icon"><i :class="item.iconCls"></i></div>
+        </mt-cell>
+      <!--</template>-->
+    </div>
+    <mt-tabbar>
+      <div style="height: 60px;margin: auto"><div style="font-style: normal;font-size: 20px;color: #606266;padding-top:15px">
+        群号: 599733338
+      </div></div>
+    </mt-tabbar>
+  </section>
+</template>
+
+<script>
+/* eslint-disable */
+    export default {
+        data() {
+            return {
+              selected: "",
+            }
+        },
+        methods: {
+        },
+        mounted() {
+        }
+    }
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
